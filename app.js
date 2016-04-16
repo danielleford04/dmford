2836e3fac4821c65b6cd9a2c6b4486134405ec8d
danielleford04 <danielleford04@gmail.com>
2016-04-16 13:12:11 -0600

    bio height on mobile

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/meditation', function(req, res){
 });
 
 // Creating Server and Listening for Connections \\
-var port = 80
+var port = 3000
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
